b59b972d5153b14dff3bbef03885e7aa35f86d3a
Bryn Beaudry <babeaudry@gmail.com>
2017-09-07 13:35:57 -0700

    student class tostring added

--- Student.php
+++ Student.php
@@ -21,6 +21,15 @@ class Student
   function add_grade($grade) {
     $this->grades[] = $grade;
   }
+
+  function toString() {
+    $result = $this->first_name . ' ' . $this->surname;
+    $result .= ' ('.$this->average().")\n";
+    foreach($this->emails as $which=>$what)
+    $result .= $which . ': '. $what. "\n";
+    $result .= "\n";
+    return '<pre>'.$result.'</pre>';
+  }
 }
 
 ?>
