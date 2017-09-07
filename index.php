393ba1b5dd0eb3eac8f6900693264525e81a887c
Bryn Beaudry <babeaudry@gmail.com>
2017-09-07 13:26:25 -0700

    setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello Out there</title>
+  </head>
+  <body>
+    <p>Hello out there... I live</p>
+    <p>-Bryn</p>
+  </body>
+</html>
